e7c100f5870d006d8ca474feacc9a217803070b2
Keve Lajsz <kevelajsz@icloud.com>
2022-09-13 12:56:48 +0200

    Első labor

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Player player1 = new Player("Tom", 100, 50, 0, 0);
+        Player player2 = new Player("David", 100, 50, 0, 0);
+
+//        Hash comparison
+        System.out.println(player1);
+        System.out.println(player2);
+        System.out.println(player1 == player2);
+
+//        Void methods
+        System.out.println(player1);
+        player1.moveUp(3);
+        System.out.println(player1);
     }
 }
---
+++ src/Player.java
@@ -0,0 +1,95 @@
+import java.util.Objects;
+
+public class Player {
+    private String name;
+    private int health;
+    private int armour;
+    private int x, y;
+
+    public Player(String name, int health, int armour, int x, int y) {
+        this.name = name;
+        this.health = health;
+        this.armour = armour;
+        this.x = x;
+        this.y = y;
+    }
+
+    public Player() {
+        this.name = "Tom";
+        this.health = 100;
+        this.armour = 40;
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getArmour() {
+        return armour;
+    }
+
+    public void setArmour(int armour) {
+        this.armour = armour;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "name='" + name + '\'' +
+                ", health=" + health +
+                ", armour=" + armour +
+                ", x=" + x +
+                ", y=" + y +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Player player = (Player) o;
+        return (
+                health == player.health && armour == player.armour &&
+                        x == player.x && y == player.y && name.equals(player.name)
+        );
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, health, armour, x, y);
+    }
+
+    public void moveUp(int amount) {
+        this.y -= amount;
+    }
+}
